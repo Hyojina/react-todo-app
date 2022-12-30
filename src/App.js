29dd0337ced075b6ec98bd28fb1f8d0b576315f6
Hyojina <hjkim3009@gmail.com>
2022-12-30 17:58:49 +0900

    feat: Filter 메소드를 사용해서 할 일 목록 지우기

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 import React, { Component } from "react";
 
 export class App extends Component {
-  todoData = [
-    { id: "1", title: "공부하기", completed: true },
-    { id: "2", title: "청소하기", completed: false },
-  ];
+  // constructor라고 안써도 됨
+  state = {
+    todoData: [
+      { id: "1", title: "공부하기", completed: true },
+      { id: "2", title: "청소하기", completed: false },
+    ],
+    value: "",
+  };
 
   btnStyle = {
     color: "#fff",
@@ -17,12 +21,19 @@ export class App extends Component {
     float: "right",
   };
 
+  // 동적으로 바꾸기 위해 listStyle은 함수로 생성
   listStyle = () => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
       textDecoration: "none",
     };
+  };
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    console.log("newTodoData", newTodoData);
+    this.setState({ todoData: newTodoData });
   };
 
   render() {
@@ -32,11 +43,16 @@ export class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.listStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={data.completed} />{" "}
               {data.title}
-              <button style={this.btnStyle}>X</button>
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleClick(data.id)}
+              >
+                X
+              </button>
             </div>
           ))}
         </div>

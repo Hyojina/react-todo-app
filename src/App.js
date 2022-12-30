0d96b47b6fcc249779fcdba29b9f3d5a92869780
Hyojina <hjkim3009@gmail.com>
2022-12-30 21:47:49 +0900

    feat: 컴포넌트 생성하기 - Form

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import Lists from "./components/Lists";
+import Form from "./components/Form";
 
 export default function App() {
   const [todoData, setTodoData] = useState([
     { id: "1", title: "공부하기", completed: true },
     { id: "2", title: "청소하기", completed: false },
   ]);
-  const [value, setValue] = useState("");
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+  const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -35,21 +33,7 @@ export default function App() {
         </div>
         {/* 이런식으로 useState를 각각 나누어서 내려줄 수 있음!!!! */}
         <Lists todoData={todoData} setTodoData={setTodoData} />
-        <form style={{ display: "flex" }} onSubmit={(e) => handleSubmit(e)}>
-          <input
-            type="text"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야할 일을 입력하세요."
-            value={value}
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
     </div>
   );

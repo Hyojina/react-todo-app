ad8f12488a159e30a9fa4447a3f812562e23ada7
Hyojina <hjkim3009@gmail.com>
2023-01-02 18:30:20 +0900

    feat: 할 일 리스트 모두 지우기 버튼 생성

--- src/App.js
+++ src/App.js
@@ -25,11 +25,16 @@ export default function App() {
     setValue("");
   };
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         {/* 이런식으로 useState를 각각 나누어서 내려줄 수 있음!!!! */}
         <Lists todoData={todoData} setTodoData={setTodoData} />
